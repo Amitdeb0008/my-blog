e88a24551da89d03d17953950d9ef222ec127df5
Amitdeb Bhattacharya <amitdebbhattacharya@amitdebs-mbp.myfiosgateway.com>
2020-03-29 10:02:41 -0400

    First commit

---
+++ src/build/precache-manifest.f07f5e24770e4a260d6ddb58f6127414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6de075b6a222dcfe613eed61045d27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075449589792db2371a1",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "b3ccf8efaa43bbc0ce74",
+    "url": "/static/js/2.6a1dd8a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6a1dd8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075449589792db2371a1",
+    "url": "/static/js/main.6c3a07ad.chunk.js"
+  },
+  {
+    "revision": "921ac687539ca31687de",
+    "url": "/static/js/runtime-main.7c2b1bab.js"
+  }
+]);
